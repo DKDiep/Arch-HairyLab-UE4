eeb8b2e5227b663970fe4ae693131da662612187
dillon_diep <dd13282@my.bristol.ac.uk>
2016-06-09 17:05:50 +0100

    Refactor BP controller to use functions, add base C++ class for game state

---
+++ Source/ArchsHairyLab/Public/HairLabGameState.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameState.h"
+#include "HairLabGameState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ARCHSHAIRYLAB_API AHairLabGameState : public AGameState
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
