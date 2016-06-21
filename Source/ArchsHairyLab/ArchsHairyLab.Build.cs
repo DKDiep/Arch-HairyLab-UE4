46743099a40dd3cb18a682a9f031a67af971aa2d
dillon_diep <dd13282@my.bristol.ac.uk>
2016-06-21 21:13:35 +0100

    Add procedural mesh module to build resolving issues on project startup

--- Source/ArchsHairyLab/ArchsHairyLab.Build.cs
+++ Source/ArchsHairyLab/ArchsHairyLab.Build.cs
@@ -6,9 +6,9 @@ public class ArchsHairyLab : ModuleRules
 {
 	public ArchsHairyLab(TargetInfo Target)
 	{
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "ProceduralMeshComponent" });
 
-		PrivateDependencyModuleNames.AddRange(new string[] {  });
+        PrivateDependencyModuleNames.AddRange(new string[] {  });
 
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
