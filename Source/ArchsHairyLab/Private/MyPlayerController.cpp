3ae380a87a4d34fd80179d09f511ad627e1c08f2
ArchArch <dd13282@my.bristol.ac.uk>
2016-07-02 02:07:47 +0100

    Add C++ mesh data for default start, middle, end components

--- Source/ArchsHairyLab/Private/MyPlayerController.cpp
+++ Source/ArchsHairyLab/Private/MyPlayerController.cpp
@@ -9,6 +9,47 @@ AMyPlayerController::AMyPlayerController(const FObjectInitializer& ObjectInitial
 	StartMeshData = NewObject<UProceduralMeshData>();
 	MiddleMeshData = NewObject<UProceduralMeshData>();
 	EndMeshData = NewObject<UProceduralMeshData>();
+
+	// Create default mesh data for start, middle, end components
+	StartMeshData->Vertices.Add(FVector(-10.0f, 50.0f, 0.0f));
+	StartMeshData->Vertices.Add(FVector(10.0f, 50.0f, 0.0f));
+	StartMeshData->Vertices.Add(FVector(-25.0f, 0.0f, 0.0f));
+	StartMeshData->Vertices.Add(FVector(25.0f, 0.0f, 0.0f));
+	StartMeshData->Triangles.Add(0);
+	StartMeshData->Triangles.Add(1);
+	StartMeshData->Triangles.Add(2);
+	StartMeshData->Triangles.Add(1);
+	StartMeshData->Triangles.Add(2);
+	StartMeshData->Triangles.Add(3);
+	StartMeshData->UVs.Add(FVector2D(0.0f, 1.0f));
+	StartMeshData->UVs.Add(FVector2D(1.0f, 1.0f));
+	StartMeshData->UVs.Add(FVector2D(0.0f, 0.0f));
+	StartMeshData->UVs.Add(FVector2D(1.0f, 0.0f));
+
+	MiddleMeshData->Vertices.Add(FVector(-25.0f, 50.0f, 0.0f));
+	MiddleMeshData->Vertices.Add(FVector(25.0f, 50.0f, 0.0f));
+	MiddleMeshData->Vertices.Add(FVector(-25.0f, 0.0f, 0.0f));
+	MiddleMeshData->Vertices.Add(FVector(25.0f, 0.0f, 0.0f));
+	MiddleMeshData->Triangles.Add(0);
+	MiddleMeshData->Triangles.Add(1);
+	MiddleMeshData->Triangles.Add(2);
+	MiddleMeshData->Triangles.Add(1);
+	MiddleMeshData->Triangles.Add(2);
+	MiddleMeshData->Triangles.Add(3);
+	MiddleMeshData->UVs.Add(FVector2D(0.0f, 1.0f));
+	MiddleMeshData->UVs.Add(FVector2D(1.0f, 1.0f));
+	MiddleMeshData->UVs.Add(FVector2D(0.0f, 0.0f));
+	MiddleMeshData->UVs.Add(FVector2D(1.0f, 0.0f));
+
+	EndMeshData->Vertices.Add(FVector(-25.0f, 50.0f, 0.0f));
+	EndMeshData->Vertices.Add(FVector(25.0f, 50.0f, 0.0f));
+	EndMeshData->Vertices.Add(FVector(0.0f, 0.0f, 0.0f));
+	EndMeshData->Triangles.Add(0);
+	EndMeshData->Triangles.Add(1);
+	EndMeshData->Triangles.Add(2);
+	EndMeshData->UVs.Add(FVector2D(0.0f, 1.0f));
+	EndMeshData->UVs.Add(FVector2D(1.0f, 1.0f));
+	EndMeshData->UVs.Add(FVector2D(0.5f, 0.0f));
 }
 
 
